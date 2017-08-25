bc8e237e25cba40850642db2e3364cebdb44ee46
Ivan Svet <ivan.svet@amdaris.com>
2017-08-25 16:59:48 +0300

    - Some unimportant stuff;

---
+++ CollatzConjectureStepCounter/CollatzConjectureStepCounter/CollatzCalculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CollatzConjectureStepCounter
+{
+    class CollatzCalculator
+    {
+        public Double NumberInQuestion { get; set; }
+
+        public CollatzCalculator(double numberInQuestion)
+        {
+            NumberInQuestion = numberInQuestion;
+        }
+
+        internal List<double> CalculateStepsRequired()
+        {
+            List<double> steps = new List<double>();
+
+
+            return steps;
+        }
+    }
+}
--- CollatzConjectureStepCounter/CollatzConjectureStepCounter/Form1.Designer.cs
+++ CollatzConjectureStepCounter/CollatzConjectureStepCounter/Form1.Designer.cs
@@ -28,12 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(157, 26);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Go";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(12, 26);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(113, 20);
+            this.numericUpDown1.TabIndex = 1;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Enabled = false;
+            this.richTextBox1.Location = new System.Drawing.Point(13, 62);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(644, 511);
+            this.richTextBox1.TabIndex = 2;
+            this.richTextBox1.Text = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(669, 585);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.button1);
+            this.MaximumSize = new System.Drawing.Size(685, 624);
+            this.MinimumSize = new System.Drawing.Size(685, 624);
+            this.Name = "Form1";
+            this.Text = "Step Counter";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
     }
 }
 
